1d2e8d86c602acaa11c8d9d227915617da0cc463
Hsu <44192532+vagr8@users.noreply.github.com>
2019-01-10 16:08:05 +0800

    Update Control_White.cpp

--- Control_White.cpp
+++ Control_White.cpp
@@ -5,18 +5,21 @@ const int pwn_a = 3;  // pin motor A output
 const int dir_b = 13; // pin motor B direction
 const int pwn_b = 11; // pin motor B output
 
+const int ledPin[4] = {2, 4, 6, 7}; // Suppose LED pin = 2 4 6 7
+
 const int s = 14; // pin for switch
 
 const int IRpin[4] = {4, 16, 15, 5}; //pin for IR sensor
 
 const int displayer[4] = {10, 9 , 8, 1}; // pin for 7-segment display 
 
-int black[4] = {400, 500, 400, 580};    // thershold for black. If input value < black[], it's black
+int black[4] = {400, 480, 400, 100};    // thershold for black. If input value < black[], it's black
 int white[4] = {0, 0, 0, 0};            // determine color detected. white = 1, black = 0
 
 int i;
 int pv[4] = {0, 0, 0, 0}; // value read from IR sensor
 boolean sw = 0;           //switch value 
+int LEDcount = 0;
 
 void show_status(int c); // show 0 1 2 3, depends on current movement
 void pc( int a[]);
@@ -25,12 +28,19 @@ void run();
 void stop();
 void black_or_white(int a[]);
 void RightAngleHandler( );
+void ledDisplay();
+void reset(int a[]);
 
 int tcounter = 0; // timer counter 
+int ledconter = 0; // ledcounter
 
 // function ti countes time.
 void ti(){
   tcounter++;
+}
+
+void ledti(){
+  ledconter++;
 }
 
 void setup(){
@@ -40,10 +50,11 @@ void setup(){
   pinMode(dir_b, OUTPUT);
   pinMode(pwn_b, OUTPUT);
 
-  pinMode(displayer[0], OUTPUT);
-  pinMode(displayer[1], OUTPUT);
-  pinMode(displayer[2], OUTPUT);
-  pinMode(displayer[3], OUTPUT);
+//set displayer pin and led pin
+  for(i = 0; i < 4; i++){
+    pinMode(displayer[i], OUTPUT);
+    pinMode(ledPin[i], OUTPUT);
+  }
 
   pinMode(s, INPUT_PULLUP);
 
@@ -63,6 +74,11 @@ void setup(){
 }
 
 void loop(){  
+
+    if( ledconter > 250 ){
+      ledconter = 0;
+      ledDisplay();
+    }
   
     if(tcounter > 10)         // read value every 10 ms 
     {
@@ -97,6 +113,7 @@ void pc(int a[]){
 }
 
 void black_or_white(int r[]){
+  int i;
   for(i = 0; i < 4; i++){
     if(r[i] < black[i]){
       white[i] = 0;
@@ -167,6 +184,7 @@ void RightAngleHandler(){
 // STOP = 0; Forward = 1; Turn Right = 2; Turn left = 3;
 void show_status( int c ){
  int b[4] = {0, 0, 0, 0};
+ int i;
 
  switch(c){
 
@@ -203,4 +221,48 @@ void show_status( int c ){
     digitalWrite(displayer[i], b[i]);
   }
 
+ }
+
+ // Masami don't like to take shower
+
+ void ledDisplay(){
+   int i;
+   int bit[4];
+
+   LEDcount++;
+   if(LEDcount > 4){
+     LEDcount = 0;
+   }
+
+  reset(bit);
+  switch(LEDcount){
+    case 0: 
+    bit[0] = 1;
+    break;
+
+    case 1: 
+    bit[1] = 1;
+    break;
+    
+    case 2: 
+    bit[2] = 1;
+    break;
+    
+    case 3: 
+    bit[3] = 1;
+    break;
+  }
+
+  for(i = 0; i < 4; i++){
+    digitalWrite( ledPin[i], !bit[i] );
+  }
+  
+ }
+
+ void reset(int a[]){
+   int i;
+
+  for(i = 0; i<4 ;i++){
+    a[i] = 0;
+  }
  }
